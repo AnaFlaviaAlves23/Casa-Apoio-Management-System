d2275bb5db5b56f1422807422a6325ae6d949773
ANA FLÁVIA <ana.flavia.alves@ufv.br>
2026-03-25 13:31:45 -0300

    Correcao final das rotas do dashboard

--- danielle/people/urls.py
+++ danielle/people/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path, include
 from people import views
-
-# ViewSet class based views
+from people.views.dashboard import dashboard  # Importando a sua função do dashboard
 from rest_framework.routers import DefaultRouter
 
+# Configuração das rotas da API (ViewSet)
 router = DefaultRouter()
 router.register('people', views.PersonViewSet)
 router.register('checkins', views.CheckinViewSet)
-router.register('patient_companion_checkin',
-                views.PatientCompanionCheckinViewSet)
+router.register('patient_companion_checkin', views.PatientCompanionCheckinViewSet)
 router.register('home_services', views.HomeServicesViewSet)
 router.register('professional_services', views.ProfessionalServicesViewSet)
-# router.register('checkins', views.CheckinViewSet)
 
+# O segredo está aqui: as rotas que o Django realmente "enxerga"
 urlpatterns = [
-    path('', include(router.urls)),
+    path('dashboard/', dashboard, name='dashboard'),  # Criando o caminho para o dashboard!
+    path('', include(router.urls)),                   # Incluindo as rotas da API
 ]
